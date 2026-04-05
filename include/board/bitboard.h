90380278825de4fe6790c8816c2f024ebd36dd63
TH3RDHOUR <daltocarter@outlook.com>
2026-04-04 23:19:35 -0500

    Fixing comments and function definition locations.

--- include/board/bitboard.h
+++ include/board/bitboard.h
@@ -8,32 +8,20 @@ namespace BitBoard
 {
     // Returns Unsigned Long Long shifted left by square bits.
     // This is a bitboard w/ only the bit for that square set to 1.
-    // Used for set/clear/get_bit.
+    // Used for set/clear/get_bit to do bitwise operations on bitboard.
     inline uint64_t mask(int square) { return 1ULL << square; }
 
     // True if piece is at square.
     inline bool get_bit(uint64_t bb, int square) { return bb & mask(square); }
 
-    // OR the bit at position square.
+    // OR the bitboard with the mask using square.
     inline void set_bit(uint64_t& bb, int square) { bb |= mask(square); }
 
-    // AND the bit at position square.
+    // AND the bitboard with the inverted mask using square.
+    // With (~) it will flip all bits (all 1s except the target bit which is zero).
     inline void clear_bit(uint64_t& bb, int square) { bb &= ~mask(square); }
 
-    // Make sure the trailing zero function is called for the correct compiler & system.
-    inline int bit_scan_forward(uint64_t bb)
-    {
-    // MSVC specific (__builtin_ctzll doesn't exist).
-    #if defined(_MSC_VER)
-        unsigned long index;
-        _BitScanForward64(&index, bb);
-        return static_cast<int>(index);
-    // GCC/Clang specific.
-    #else
-        return __builtin_ctzll(bb);
-    #endif
-    }
-
+    int bit_scan_forward(uint64_t bb);
     int pop_lsb(uint64_t& bb);
 }
 
--- src/board/bitboard.cpp
+++ src/board/bitboard.cpp
@@ -1,5 +1,19 @@
 #include "board/bitboard.h"
 
+// Make sure the trailing zero function is called for the correct compiler & system.
+int BitBoard::bit_scan_forward(uint64_t bb)
+{
+    // MSVC specific (__builtin_ctzll doesn't exist).
+    #if defined(_MSC_VER)
+        unsigned long index;
+        _BitScanForward64(&index, bb);
+        return static_cast<int>(index);
+    // GCC/Clang specific.
+    #else
+        return __builtin_ctzll(bb);
+    #endif
+}
+
 // Pop Least significant bit (rightmost 1 in the bitboard).
 // Also known as the piece on the "lowest index" square.
 // Allows for quicker iteration of moves for every piece.
@@ -16,5 +30,6 @@ int BitBoard::pop_lsb(uint64_t& bb)
     // Flips all bits from the LSB to the right.
     bb &= bb - 1;
 
+    // Return index of the LSB.
     return square;
 }
